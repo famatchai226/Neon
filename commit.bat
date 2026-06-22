2a004cf646ca41926060a2f7b2dd7dd7b8f3c78f
famatchai226 <famatchai226@gmail.com>
2026-06-22 00:42:39 +0000

    Remove temporary commit.bat

--- commit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d c:\laragon\www\Neon
-git add .
-git commit -m "Initial commit: Project setup with .gitignore and Contexte.md"
---
+++ commit2.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d c:\laragon\www\Neon
+git add .
+git commit -m "Remove temporary commit.bat"
